54795581c83ed33be882c03a452bdedb7196a964
archana-3001 <archanakumari.int@delhivery.com>
2023-02-01 10:36:19 +0530

    list users with attributes

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,31 +1,55 @@
 //  create/update/read/delete users from the system
-import { Router, response, query } from "express";
+import e, { Router, response, query } from "express";
 import { request } from "http";
 import userValidation from "../middleware/userValidation";
 import addUser from "../controller/users";
 import {getConnection } from '../db'; 
+import { FORMERR } from "dns";
 
 
 const userRouter=Router();
 export const cluster=getConnection();
 
+// GET USER WITH USER ID
 userRouter.get('/', (request,  response)=>{
-    const query = 'SELECT * from users';
-    var users=[];
-    const res=cluster.execute(query);
-    res.then(val=>{
-        val.rows.forEach(val=>{
-            console.log(val);
-            users.push(val);
-        })
-    });
-    console.log(users)
-    response.send("hello from all users !!!!");
+    
+    console.log(request.query);
+    const keys= Object.keys(request.query);
+    if(keys.length!=0){
+        for (const [key, value] of Object.entries(request.query)) {
+            //console.log(`${key}: ${value}`);
+            // console.log(typeof(key), typeof(value));
+            var query = `SELECT * from users WHERE ${key}= '${value}' ALLOW FILTERING;`;
+            if(`${key}`=='ID'){
+                query = `SELECT * from users WHERE ${key}= ${value} ALLOW FILTERING;`;
+            }
+            console.log(query);
+            const res=cluster.execute(query);
+            res.then(val=>{
+                if(val.rows.length!=0){
+                    return response.status(200).json({msg: val.rows});
+                }
+                else{
+                    return response.status(404).json({error: "user not found!!"});
+                }
+            }).catch(err=>{
+                return response.status(404).json({
+                    err: err
+                })
+            })
+
+        }
+    }else{
+        return response.status(404).json({
+            msg: "NO parameters passed in url"
+        });
+    }
 });
 
 
 userRouter.patch('/', (request, response) =>{
     // console.log(request.query);
+    
     response.send("attribute added !!!");
 });
 
